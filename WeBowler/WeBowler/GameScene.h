76582cde5a9248fe8f8135675b81358bf7138351
sbarton <scottebarton@gmail.com>
2014-10-28 13:36:14 -0400

    Initial checkin project skeleton

---
+++ WeBowler/WeBowler/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  WeBowler
+//
+
+//  Copyright (c) 2014 Sage. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ WeBowler/WeBowler/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  WeBowler
+//
+
+//  Copyright (c) 2014 Sage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
