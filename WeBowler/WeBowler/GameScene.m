00f91aff1a57104f723b56f66ffb6fffe96b9373
Vasin Soparkdithapong <vasin.soparkdithapong@sage.com>
2014-10-29 16:01:37 -0400

    Change game to portrait mode

--- WeBowler/WeBowler/GameScene.m
+++ WeBowler/WeBowler/GameScene.m
@@ -161,7 +161,7 @@ typedef NS_OPTIONS(uint32_t, PMPhysicsCategory) {
     
     // Add Label
     _hitLabel = [SKLabelNode labelNodeWithFontNamed:@"Courier Bold"];
-    _hitLabel.position = CGPointMake(100, 700);
+    _hitLabel.position = CGPointMake(100, 900);
     _hitLabel.fontSize = 40.0;
     _hitLabel.fontColor = [SKColor blackColor];
     _hitLabel.text = @"Hits: 0";
@@ -185,7 +185,7 @@ typedef NS_OPTIONS(uint32_t, PMPhysicsCategory) {
     SKNode* node = [self findBBall];
     
     //If bball is off the screen, add another
-    if(node.position.y < 0 || node.position.y > 168 || node.position.x < 0 || node.position.x > 1024)
+    if(node.position.y < 0 || node.position.y > 1024 || node.position.x < 0 || node.position.x > 768)
     {
         [node removeFromParent];
         [self addBBall];
@@ -224,6 +224,7 @@ typedef NS_OPTIONS(uint32_t, PMPhysicsCategory) {
 {
     SKSpriteNode *gutter1 = [SKSpriteNode spriteNodeWithTexture:_gutter1Texture];
     SKSpriteNode *gutter2 = [SKSpriteNode spriteNodeWithTexture:_gutter2Texture];
+    gutter1.si
     
     gutter1.name = @"gutter1";
     
@@ -235,7 +236,7 @@ typedef NS_OPTIONS(uint32_t, PMPhysicsCategory) {
     gutter1.physicsBody.categoryBitMask = GUTTER1Cat;
     gutter1.physicsBody.collisionBitMask = BBALLCat | EdgeCat;
     gutter1.physicsBody.contactTestBitMask = BBALLCat;
-    gutter1.position = CGPointMake(224, 100);
+    gutter1.position = CGPointMake(100, 0);
     
     
     gutter2.name = @"gutter2";
@@ -248,7 +249,7 @@ typedef NS_OPTIONS(uint32_t, PMPhysicsCategory) {
     gutter2.physicsBody.categoryBitMask = GUTTER1Cat;
     gutter2.physicsBody.collisionBitMask = BBALLCat | EdgeCat;
     gutter2.physicsBody.contactTestBitMask = BBALLCat;
-    gutter2.position = CGPointMake(800, 100);
+    gutter2.position = CGPointMake(668, 0);
     
     //gutter1.position = CGPointMake(gutter1.size.width/2, gutter1.size.height/2);
     //gutter2.position = CGPointMake(gutter1.size.width/2, gutter1.size.height/2);
